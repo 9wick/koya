5a85c3c67adfa30d54ce09558b89151a0918bae0
KoheiKido <kido@9wick.com>
2026-05-06 17:43:13 +0900

    feat(command): export command module
    Verified: precommit-verify ✕

--- packages/command/src/index.ts
+++ packages/command/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export { Command } from './decorators/command';
+export { getCommandMetadata, type CommandMetadata } from './internal/metadata';
+export type {
+  ArgsDefinition,
+  CommandClass,
+  CommandContext,
+  InferArgs,
+  InferOptions,
+  OptionsDefinition,
+} from './types';
